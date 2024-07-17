d1df290dbda2c439d452786e4b6812fe303aed80
Fabio Riva <f.riva@sotefin.ch>
2024-07-17 22:25:29 +0100

    building B def updated

--- menlo/b/def.js
+++ menlo/b/def.js
@@ -5,7 +5,7 @@ exports.APS = 'menlo'
 //     : process.env.PRODUCTION_SERVER
 exports.HTTP = 9100
 exports.PLC = {
-  ip: '192.168.83.102',
+  ip: '192.168.183.102',
   rack: 0,
   slot: 1,
   polling_time: 999
@@ -14,16 +14,23 @@ exports.PLC = {
 exports.QUEUE_LEN = 5
 
 const DB_DATA = 506
-const DB_DATA_LEN = 104
+const DB_DATA_LEN = 106
 exports.DB_DATA_INIT_DEVICE = 0
 exports.DB_DATA_INIT_EXIT_QUEUE = 36
-exports.DB_DATA_INIT_SWAP_QUEUE = 70
+exports.DB_DATA_INIT_SWAP_QUEUE = 72
 
 exports.DATA_READ = {
   area: 0x84,
   dbNumber: DB_DATA,
   start: 0,
   amount: DB_DATA_LEN,
+  wordLen: 0x02
+}
+exports.REQ_EXIT = {
+  area: 0x84,
+  dbNumber: DB_DATA,
+  start: 70,
+  amount: 2,
   wordLen: 0x02
 }
 exports.REQ_SWAP = {
